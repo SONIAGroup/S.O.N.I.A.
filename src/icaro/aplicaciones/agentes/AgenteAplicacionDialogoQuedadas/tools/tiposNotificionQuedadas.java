c29a75a861151c082a588bc1851cdf60025ea3e5
Mariano Hernandez <marianoh@ucm.es>
2016-04-24 16:36:38 +0200

    Intento de meter reglas 1...fallido

--- src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoQuedadas/tools/tiposNotificionQuedadas.java
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoQuedadas/tools/tiposNotificionQuedadas.java
@@ -8,15 +8,15 @@ package icaro.aplicaciones.agentes.AgenteAplicacionDialogoQuedadas.tools;
  */
 public class tiposNotificionQuedadas {
 	
-	public static String inicioPeticion	 = "InicioPeticion";
-	public static String numeroOtroGrupo = "numeroOtroGrupo";
-	public static String edadOtroGrupo	 = "edadOtroGrupo";
-	public static String sexoOtroGrupo   = "sexoOtroGrupo";
-	public static String que			 = "que";
-	public static String donde			 = "donde";
-	public static String cuando			 = "cuando";
-	public static String si				 = "si";
-	public static String no				 = "no";
-	public static String sesion			 = "session";
+	public static String inicioPeticion		  = "InicioPeticion";
+	public static String integrantesOtroGrupo = "integrantesOtroGrupo";
+	public static String edadOtroGrupo	 	  = "edadOtroGrupo";
+	public static String sexoOtroGrupo   	  = "sexoOtroGrupo";
+	public static String que			 	  = "que";
+	public static String donde			 	  = "donde";
+	public static String cuando			 	  = "cuando";
+	public static String si				 	  = "si";
+	public static String no				 	  = "no";
+	public static String sesion			 	  = "session";
 
 }
