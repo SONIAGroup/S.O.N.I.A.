1e14152e707a6de49affe25425ede071328de1e9
Jorge Casas Hernán <jorcasas@ucm.es>
2016-05-07 19:23:53 +0200

    Añadido MensajeGrupo y creado recurso persistencia de estos objetos

---
+++ src/icaro/aplicaciones/informacion/gestionQuedadas/MensajeGrupo.java
@@ -0,0 +1,30 @@
+package icaro.aplicaciones.informacion.gestionQuedadas;
+
+public class MensajeGrupo {
+	
+	static public final int CANCELAR = 0;
+	static public final int CONFIRMAR = 1;
+	static public final int SOLICITAR = 2;
+	
+	public String idGrupoEmisor;
+	public String idGrupoDestinatario;
+	public int mensaje;
+
+	public MensajeGrupo(String idGrupoEmisor, String idGrupoDestinatario, int mensaje) {
+		this.idGrupoEmisor = idGrupoEmisor;
+		this.idGrupoDestinatario = idGrupoDestinatario;
+		this.mensaje = mensaje;
+	}
+
+	public String getIdGrupoEmisor() {
+		return idGrupoEmisor;
+	}
+	
+	public String getIdGrupoDestinatario() {
+		return idGrupoDestinatario;
+	}
+	
+	public int getMensaje() {
+		return mensaje;
+	}
+}
---
+++ src/icaro/aplicaciones/recursos/persistenciaMensajesGrupo/imp/PersistenciaMensajesGrupoImp.java
@@ -0,0 +1,24 @@
+package icaro.aplicaciones.recursos.persistenciaMensajesGrupo.imp;
+
+import icaro.aplicaciones.informacion.IOUtils;
+import icaro.aplicaciones.informacion.gestionQuedadas.MensajeGrupo;
+import java.io.Serializable;
+import java.util.Map;
+
+public class PersistenciaMensajesGrupoImp implements Serializable {
+
+	private static final long serialVersionUID = 6471866360539062076L;
+	private static final String MENSAJES_PATH = "mensajes";
+	private static Map<String, MensajeGrupo> tablaMensajesGrupo = IOUtils.read(MENSAJES_PATH);
+
+	public static void insertarMensajeGrupo(MensajeGrupo mgr) {
+		tablaMensajesGrupo.put(mgr.getIdGrupoDestinatario(), mgr);
+		IOUtils.write(MENSAJES_PATH, tablaMensajesGrupo);
+	}
+
+	public static MensajeGrupo obtenerMensajeGrupo(String idGrupo) {
+		return tablaMensajesGrupo.get(idGrupo);
+
+	}
+
+}
